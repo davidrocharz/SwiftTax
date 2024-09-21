a026ee374af78ff7f36e7d8dabc39d000c5dcc47
David Rocharz <davidrocharz@gmail.com>
2024-09-21 13:08:21 -0400

    Initial Commit

---
+++ SwiftTax/SwiftTaxApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftTaxApp.swift
+//  SwiftTax
+//
+//  Created by David Rocharz on 9/21/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftTaxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftTaxTests/SwiftTaxTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftTaxTests.swift
+//  SwiftTaxTests
+//
+//  Created by David Rocharz on 9/21/24.
+//
+
+import Testing
+@testable import SwiftTax
+
+struct SwiftTaxTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
